a361691d70e8d6bae5e3eed0522f19e5da947221
Oni_DOS <42125158+Oni-DOS@users.noreply.github.com>
2026-05-04 22:44:36 +0300

    Add desktop-notifications path; remove ts-ignore
    Update tsconfig to add baseUrl and path aliases mapping 'desktop-notifications' (and wildcard) to a noop implementation so TypeScript can resolve notification imports. Remove a stray '// @ts-ignore' in main.ts before importing notification helpers.

--- app/src/main-process/main.ts
+++ app/src/main-process/main.ts
@@ -43,7 +43,6 @@ import {
 } from '../lib/get-architecture'
 import { buildSpellCheckMenu } from './menu/build-spell-check-menu'
 import { getMainGUID, saveGUIDFile } from '../lib/get-main-guid'
-// @ts-ignore
 import {
   getNotificationsPermission,
   requestNotificationsPermission,
